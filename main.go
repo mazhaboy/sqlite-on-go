d1c2d2fa4f4a5c0ffe927a6953cf378b3d1972a3
Temirlan <68460327+mazhaboy@users.noreply.github.com>
2022-05-05 01:37:53 +0600

    part(2/13): add statements

--- main.go
+++ main.go
@@ -10,25 +10,41 @@ import (
 
 func main() {
 	// Hardcoded repl commands
-	commands := map[string]interface{}{
+	metaCommands := map[string]interface{}{
 		".help":  displayHelp,
 		".clear": clearScreen,
+	}
+
+	statements := map[string]interface{}{
+		"select": selectState,
+		"insert": insertState,
 	}
 	// Begin the repl loop
 	reader := bufio.NewScanner(os.Stdin)
 	printPrompt()
 	for reader.Scan() {
 		text := cleanInput(reader.Text())
-		if command, exists := commands[text]; exists {
-			// Call a hardcoded function
-			command.(func())()
-		} else if strings.EqualFold(".exit", text) {
-			// Close the program on the exit command
-			return
+		if strings.EqualFold(".", string(reader.Bytes()[0])) {
+			if command, exists := metaCommands[text]; exists {
+				// Call a hardcoded function
+				command.(func())()
+			} else if strings.EqualFold(".exit", text) {
+				// Close the program on the exit command
+				return
+			} else {
+				// Pass the command to the parser
+				handleCmd(text)
+			}
 		} else {
-			// Pass the command to the parser
-			handleCmd(text)
+			if statement, exists := statements[text]; exists {
+				// Call a hardcoded function
+				statement.(func())()
+			} else {
+				// Pass the command to the parser
+				handleCmd(text)
+			}
 		}
+
 		printPrompt()
 	}
 	// Print an additional line if we encountered an EOF character
@@ -78,4 +94,12 @@ func cleanInput(text string) string {
 	output := strings.TrimSpace(text)
 	output = strings.ToLower(output)
 	return output
+}
+
+func insertState() {
+	fmt.Println("INSERT - insert to database")
+}
+
+func selectState() {
+	fmt.Println("SELECT - select from database")
 }
